a61f175ea91326d1d1c9df9bcb7ec647bb0934a3
Vũ Long <v.longv1@vinid.net>
2020-05-18 14:33:18 +0700

    [Leetcode][May 2020] Permutation in String

---
+++ leetcode/maychallenge/check_inclusion.go
@@ -0,0 +1,32 @@
+package maychallenge
+
+func checkInclusion(s1 string, s2 string) bool {
+	const a = 'a'
+	lS1 := len(s1)
+	lS2 := len(s2)
+	s1Arr := make([]int, 26)
+	s2Arr := make([]int, 26)
+
+	for i := 0; i < lS1; i++ {
+		s1Arr[s1[i]-a]++
+	}
+	for i := 0; i < lS2; i++ {
+		s2Arr[s2[i]-a]++
+		if i >= lS1 {
+			s2Arr[s2[i-lS1]-a]--
+		}
+		if verify2CharArray(s2Arr, s1Arr) {
+			return true
+		}
+	}
+	return false
+}
+
+func verify2CharArray(a, b []int) bool {
+	for i := 0; i < 26; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
