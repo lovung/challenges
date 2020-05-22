7a38a6113b3e446ac668d40a4678732a93f44097
Vũ Long <v.longv1@vinid.net>
2020-05-22 09:36:24 +0700

    fixup! [Leetcode][May 2020] Check If It Is a Straight Line

--- leetcode/maychallenge/check_straight_line_test.go
+++ leetcode/maychallenge/check_straight_line_test.go
@@ -18,6 +18,34 @@ func Test_checkStraightLine(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "test 2",
+			args: args{
+				coordinates: [][]int{[]int{1, 2}, []int{2, 3}, []int{3, 4}, []int{4, 5}, []int{5, 7}},
+			},
+			want: false,
+		},
+		{
+			name: "test 3",
+			args: args{
+				coordinates: [][]int{[]int{1, 2}, []int{2, 2}, []int{3, 2}, []int{4, 2}},
+			},
+			want: true,
+		},
+		{
+			name: "test 4",
+			args: args{
+				coordinates: [][]int{[]int{1, 2}, []int{2, 2}, []int{3, 2}, []int{4, 3}},
+			},
+			want: false,
+		},
+		{
+			name: "test 5",
+			args: args{
+				coordinates: [][]int{[]int{1, 2}, []int{1, 3}, []int{1, 4}, []int{1, 5}},
+			},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
