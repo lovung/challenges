5b9a5c7aa1913da97f0b05c5d2ecbfe768e0262b
vulong <17726269+lovung@users.noreply.github.com>
2020-05-22 23:21:31 +0700

    [Leetcode][May 2020] Sort Characters By Frequency (#19)
    * [Leetcode][May 2020] Sort Characters By Frequency
    
    * fixup! [Leetcode][May 2020] Sort Characters By Frequency
    
    Co-authored-by: Vũ Long <v.longv1@vinid.net>

---
+++ leetcode/maychallenge/sort_characters_by_frequency_test.go
@@ -0,0 +1,43 @@
+package maychallenge
+
+import "testing"
+
+func Test_frequencySort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test 1",
+			args: args{
+				s: "tree",
+			},
+			want: "eetr",
+		},
+		{
+			name: "test 2",
+			args: args{
+				s: "cccaaa",
+			},
+			want: "cccaaa",
+		},
+		{
+			name: "test 3",
+			args: args{
+				s: "Aabb",
+			},
+			want: "bbaA",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := frequencySort(tt.args.s); got != tt.want {
+				t.Errorf("frequencySort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
