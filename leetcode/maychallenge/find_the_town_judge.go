39393c1b289551faa2bdd772b5ebf6cad933001c
Vũ Long <v.longv1@vinid.net>
2020-05-23 11:17:14 +0700

    fixup! [Leetcode][May 2020] Find the Town Judge

--- leetcode/maychallenge/find_the_town_judge.go
+++ leetcode/maychallenge/find_the_town_judge.go
@@ -1,5 +1,6 @@
 package maychallenge
 
+// Link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3325/
 func findJudge(N int, trust [][]int) int {
 	except := make([]int, N)
 	count := make([]int, N)
