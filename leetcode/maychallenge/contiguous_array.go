5cc9c67075223d1ccfec30f169a2cc55272ac441
Vũ Long <v.longv1@vinid.net>
2020-05-26 20:16:27 +0700

    [Leetcode][May 2020] Max length Contiguous Array

---
+++ leetcode/maychallenge/contiguous_array.go
@@ -0,0 +1,33 @@
+package maychallenge
+
+func findMaxLength(nums []int) int {
+	n := len(nums)
+	prefixSum := make([]int, n+1)
+	counted := make(map[int]bool)
+	sum := 0
+	for i, e := range nums {
+		if e == 0 {
+			sum--
+		} else {
+			sum++
+		}
+		prefixSum[i+1] = sum
+	}
+	max := 0
+	for i := 0; i <= n; i++ {
+		if counted[prefixSum[i]] {
+			continue
+		}
+		for j := n; j > i; j-- {
+			if prefixSum[i] == prefixSum[j] {
+				length := j - i
+				if max < length {
+					max = length
+				}
+				counted[prefixSum[i]] = true
+				break
+			}
+		}
+	}
+	return max
+}
