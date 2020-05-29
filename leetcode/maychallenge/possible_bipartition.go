78f77da1257175d3e444f20092aedb11bd7c14ce
Vũ Long <v.longv1@vinid.net>
2020-05-29 23:38:44 +0700

    fixup! [Leetcode][May 2020][Improve] Counting bits

--- leetcode/maychallenge/possible_bipartition.go
+++ leetcode/maychallenge/possible_bipartition.go
@@ -1,100 +1,33 @@
 package maychallenge
 
-// import "errors"
-
 func possibleBipartition(N int, dislikes [][]int) bool {
-	// 	var (
-	// 		m     = len(dislikes)
-	// 		gA    = make([]bool, N+1)
-	// 		gB    = make([]bool, N+1)
-	// 		queue = Queue{}
-	// 		first = true
-	// 	)
-	// 	for i := 0; i < m; i++ {
-	// 		err := process(gA, gB, dislikes[i], &queue, &first)
-	// 		if err != nil {
-	// 			return false
-	// 		}
-	// 	}
-	// 	for queue.count() > 0 {
-	// 		pair := queue.peek()
-	// 		err := process2(gA, gB, pair, &queue)
-	// 		if err != nil {
-	// 			return false
-	// 		}
-	// 		queue.pop()
-	// 	}
-	// 	return true
+	var (
+		graph = make([][]int, N+1)
+		color = make(map[int]int)
+		dfs   func(int, int) bool
+	)
+	for _, e := range dislikes {
+		graph[e[0]] = append(graph[e[0]], e[1])
+		graph[e[1]] = append(graph[e[1]], e[0])
+	}
 
+	dfs = func(node, c int) bool {
+		if val, ok := color[node]; ok {
+			return val == c
+		}
+		color[node] = c
+		for _, e := range graph[node] {
+			if !dfs(e, c^1) {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := 1; i <= N; i++ {
+		if _, ok := color[i]; !ok && !dfs(i, 0) {
+			return false
+		}
+	}
+	return true
 }
-
-// func process(gA, gB []bool, pair []int, queue *Queue, first *bool) error {
-// 	if gA[pair[0]] && gA[pair[1]] {
-// 		return errors.New("error")
-// 	}
-// 	if gB[pair[0]] && gB[pair[1]] {
-// 		return errors.New("error")
-// 	}
-
-// 	if !gA[pair[0]] && !gB[pair[0]] && !gA[pair[1]] && !gB[pair[1]] {
-// 		if *first {
-// 			gA[pair[0]] = true
-// 			gB[pair[1]] = true
-// 			*first = false
-// 		} else {
-// 			queue.push(pair)
-// 		}
-// 		return nil
-// 	}
-// 	if gA[pair[0]] {
-// 		gB[pair[1]] = true
-// 	} else if gB[pair[0]] {
-// 		gA[pair[1]] = true
-// 	} else if gA[pair[1]] {
-// 		gB[pair[0]] = true
-// 	} else if gB[pair[1]] {
-// 		gA[pair[0]] = true
-// 	} else {
-// 		return errors.New("error")
-// 	}
-// 	return nil
-// }
-
-// func process2(gA, gB []bool, pair []int, queue *Queue) error {
-// 	if gA[pair[0]] && gA[pair[1]] {
-// 		return errors.New("error")
-// 	}
-// 	if gB[pair[0]] && gB[pair[1]] {
-// 		return errors.New("error")
-// 	}
-
-// 	if gA[pair[0]] {
-// 		gB[pair[1]] = true
-// 	} else if gB[pair[0]] {
-// 		gA[pair[1]] = true
-// 	} else if gA[pair[1]] {
-// 		gB[pair[0]] = true
-// 	} else if gB[pair[1]] {
-// 		gA[pair[0]] = true
-// 	}
-// 	return nil
-// }
-
-// // Queue ...
-// type Queue [][]int
-
-// func (s Queue) peek() []int {
-// 	return s[0]
-// }
-
-// func (s Queue) count() int {
-// 	return len(s)
-// }
-
-// func (s *Queue) pop() {
-// 	*s = (*s)[1:]
-// }
-
-// func (s *Queue) push(value []int) {
-// 	*s = append(*s, value)
-// }
--- leetcode/maychallenge/possible_bipartition_test.go
+++ leetcode/maychallenge/possible_bipartition_test.go
@@ -12,38 +12,38 @@ func Test_possibleBipartition(t *testing.T) {
 		args args
 		want bool
 	}{
-		// {
-		// 	name: "test 1",
-		// 	args: args{
-		// 		N:        3,
-		// 		dislikes: [][]int{[]int{1, 2}, []int{1, 3}, []int{2, 3}},
-		// 	},
-		// 	want: false,
-		// },
-		// {
-		// 	name: "test 2",
-		// 	args: args{
-		// 		N:        10,
-		// 		dislikes: [][]int{[]int{4, 7}, []int{4, 8}, []int{2, 8}, []int{8, 9}, []int{1, 6}, []int{5, 8}, []int{1, 2}, []int{6, 7}, []int{3, 10}, []int{8, 10}, []int{1, 5}, []int{7, 10}, []int{1, 10}, []int{3, 5}, []int{3, 6}, []int{1, 4}, []int{3, 9}, []int{2, 3}, []int{1, 9}, []int{7, 9}, []int{2, 7}, []int{6, 8}, []int{5, 7}, []int{3, 4}},
-		// 	},
-		// 	want: true,
-		// },
-		// {
-		// 	name: "test 3",
-		// 	args: args{
-		// 		N:        4,
-		// 		dislikes: [][]int{[]int{1, 2}, []int{1, 3}, []int{2, 4}},
-		// 	},
-		// 	want: true,
-		// },
-		// {
-		// 	name: "test 4",
-		// 	args: args{
-		// 		N:        5,
-		// 		dislikes: [][]int{[]int{1, 2}, []int{3, 4}, []int{4, 5}, []int{3, 5}},
-		// 	},
-		// 	want: false,
-		// },
+		{
+			name: "test 1",
+			args: args{
+				N:        3,
+				dislikes: [][]int{[]int{1, 2}, []int{1, 3}, []int{2, 3}},
+			},
+			want: false,
+		},
+		{
+			name: "test 2",
+			args: args{
+				N:        10,
+				dislikes: [][]int{[]int{4, 7}, []int{4, 8}, []int{2, 8}, []int{8, 9}, []int{1, 6}, []int{5, 8}, []int{1, 2}, []int{6, 7}, []int{3, 10}, []int{8, 10}, []int{1, 5}, []int{7, 10}, []int{1, 10}, []int{3, 5}, []int{3, 6}, []int{1, 4}, []int{3, 9}, []int{2, 3}, []int{1, 9}, []int{7, 9}, []int{2, 7}, []int{6, 8}, []int{5, 7}, []int{3, 4}},
+			},
+			want: true,
+		},
+		{
+			name: "test 3",
+			args: args{
+				N:        4,
+				dislikes: [][]int{[]int{1, 2}, []int{1, 3}, []int{2, 4}},
+			},
+			want: true,
+		},
+		{
+			name: "test 4",
+			args: args{
+				N:        5,
+				dislikes: [][]int{[]int{1, 2}, []int{3, 4}, []int{4, 5}, []int{3, 5}},
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
