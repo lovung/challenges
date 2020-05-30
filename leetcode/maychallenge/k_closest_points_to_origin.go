59cb380a1b3f265449233ba785417b8b70d32cde
vulong <17726269+lovung@users.noreply.github.com>
2020-05-30 22:48:42 +0700

    [Leetcode][May 2020][Improve] K Closest points to origin (#29)
    Co-authored-by: Vũ Long <v.longv1@vinid.net>

---
+++ leetcode/maychallenge/k_closest_points_to_origin.go
@@ -0,0 +1,22 @@
+package maychallenge
+
+import "sort"
+
+func kClosest(points [][]int, K int) [][]int {
+	var (
+		n        = len(points)
+		rangeArr = make([]int, n)
+	)
+	for i, e := range points {
+		rangeArr[i] = e[0]*e[0] + e[1]*e[1]
+	}
+	sort.Ints(rangeArr)
+	maxOkRange := rangeArr[K-1]
+	var ans [][]int
+	for _, e := range points {
+		if e[0]*e[0]+e[1]*e[1] <= maxOkRange {
+			ans = append(ans, e)
+		}
+	}
+	return ans
+}
