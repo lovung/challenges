3be065ba2b15d3773138b723f053c129c78328fd
vulong <17726269+lovung@users.noreply.github.com>
2020-06-01 16:16:32 +0700

    [Leetcode][May 2020] Edit distance (#33)
    Co-authored-by: Vũ Long <v.longv1@vinid.net>

---
+++ leetcode/maychallenge/edit_distance_test.go
@@ -0,0 +1,39 @@
+package maychallenge
+
+import "testing"
+
+func Test_minDistance(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test 1",
+			args: args{
+				word1: "horse",
+				word2: "ros",
+			},
+			want: 3,
+		},
+		{
+			name: "test 2",
+			args: args{
+				word1: "abbbb",
+				word2: "abbcc",
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minDistance(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("minDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
