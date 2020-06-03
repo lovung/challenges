e81f125db96ce6a218ce4050aed9a5a9b2786854
Vũ Long <v.longv1@vinid.net>
2020-06-03 16:58:18 +0700

    [Leetcode][June 2020] Two city scheduling

---
+++ leetcode/junechallenge/two_city_scheduling_test.go
@@ -0,0 +1,29 @@
+package junechallenge
+
+import "testing"
+
+func Test_twoCitySchedCost(t *testing.T) {
+	type args struct {
+		costs [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test 1",
+			args: args{
+				costs: [][]int{{10, 20}, {30, 200}, {400, 50}, {30, 20}},
+			},
+			want: 110,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoCitySchedCost(tt.args.costs); got != tt.want {
+				t.Errorf("twoCitySchedCost() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
