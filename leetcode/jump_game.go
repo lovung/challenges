4f4b1ba738b2be5ec2e79e8749f52260e3967cb4
lovung <vulong1602@gmail.com>
2021-11-20 20:01:25 +0700

    [Leetcode] Jump Game 2 (https://leetcode.com/problems/jump-gam-iie)

--- leetcode/jump_game.go
+++ leetcode/jump_game.go
@@ -10,3 +10,26 @@ func canJump(nums []int) bool {
 	}
 	return maxReachIdx >= n-1
 }
+
+func jumpGame2(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return 0
+	}
+	maxReachIdx := nums[0]
+	nextMaxIdx := maxReachIdx
+	jumpCount := 1
+	for i := 1; i <= maxReachIdx && i < n-1; i++ {
+		if i <= nextMaxIdx {
+			if maxReachIdx < i+nums[i] {
+				maxReachIdx = i + nums[i]
+			}
+		}
+		if i == nextMaxIdx {
+			// fmt.Println(i, jumpCount, maxReachIdx)
+			jumpCount++
+			nextMaxIdx = maxReachIdx
+		}
+	}
+	return jumpCount
+}
--- leetcode/jump_game_test.go
+++ leetcode/jump_game_test.go
@@ -11,3 +11,15 @@ func TestCanJump(t *testing.T) {
 	assert.False(t, canJump([]int{3, 2, 1, 0, 4}))
 	assert.True(t, canJump([]int{1, 2, 3}))
 }
+
+func TestJumpGame2(t *testing.T) {
+	assert.Equal(t, 2, jumpGame2([]int{7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3}))
+	assert.Equal(t, 2, jumpGame2([]int{2, 3, 1, 1, 4}))
+	assert.Equal(t, 2, jumpGame2([]int{3, 2, 1, 1, 4}))
+	assert.Equal(t, 2, jumpGame2([]int{1, 2, 3}))
+	assert.Equal(t, 1, jumpGame2([]int{2, 1}))
+	assert.Equal(t, 1, jumpGame2([]int{5, 4, 3, 2, 1, 0}))
+	assert.Equal(t, 1, jumpGame2([]int{2, 0}))
+	assert.Equal(t, 1, jumpGame2([]int{2, 3, 1}))
+	assert.Equal(t, 3, jumpGame2([]int{1, 1, 1, 1}))
+}
