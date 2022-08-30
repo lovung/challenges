5a4f0ff0dbcdafb7299ad1457c64c9b635f72432
lovung <vulong1602@gmail.com>
2022-08-31 02:28:40 +0800

    [Leetcode][Medium] 310. Minimum Height Trees - TopoSort solution

--- leetcode/problems/undriectedgraph/minimum_height_trees.go
+++ leetcode/problems/undriectedgraph/minimum_height_trees.go
@@ -1,57 +1,45 @@
 package undriectedgraph
 
-import (
-	"container/heap"
-
-	"github.com/lovung/ds/heaps"
-	"github.com/lovung/ds/maths"
-)
+import "github.com/lovung/ds/queue"
 
 // Link: https://leetcode.com/problems/minimum-height-trees/
 func findMinHeightTrees(n int, edges [][]int) []int {
-	links := make([][]int, n)
+	if n == 1 {
+		return []int{0}
+	}
+	neighbors := make([]map[int]struct{}, n)
 	for i := range edges {
-		links[edges[i][0]] = append(links[edges[i][0]], edges[i][1])
-		links[edges[i][1]] = append(links[edges[i][1]], edges[i][0])
+		if neighbors[edges[i][0]] == nil {
+			neighbors[edges[i][0]] = make(map[int]struct{})
+		}
+		neighbors[edges[i][0]][edges[i][1]] = struct{}{}
+		if neighbors[edges[i][1]] == nil {
+			neighbors[edges[i][1]] = make(map[int]struct{})
+		}
+		neighbors[edges[i][1]][edges[i][0]] = struct{}{}
 	}
-	minHeight := n
-	minHeap := heaps.MinHeapWithValue[int]{}
+	leaves := queue.NewQueue[int]()
 	for i := 0; i < n; i++ {
-		// dfs instead
-		mark := make([]bool, n)
-		height := findMinHeightTreesBFS(n, links, i, mark)
-		if height < minHeight {
-			minHeight = height
+		if len(neighbors[i]) == 1 {
+			leaves.Push(i)
 		}
-		heap.Push(&minHeap, &heaps.HeapItem[int]{
-			Ref:   height,
-			Value: i,
-		})
 	}
 
-	res := make([]int, 0)
-	for item := heap.Pop(&minHeap).(*heaps.HeapItem[int]); item.Ref == minHeight; {
-		res = append(res, item.Value.(int))
-		if minHeap.Len() == 0 {
-			break
-		}
-		item = heap.Pop(&minHeap).(*heaps.HeapItem[int])
-	}
-	return res
-}
+	remainNode := n
+	for remainNode > 2 {
+		newLeaves := queue.NewQueue[int]()
+		remainNode -= leaves.Len()
+		for leaves.Len() > 0 {
+			left := leaves.Pop()
+			for k := range neighbors[left] {
+				delete(neighbors[k], left)
+				if len(neighbors[k]) == 1 {
+					newLeaves.Push(k)
+				}
+			}
 
-// return height
-func findMinHeightTreesBFS(n int, links [][]int, start int, mark []bool) int {
-	if mark[start] {
-		return 0
+		}
+		leaves = newLeaves
 	}
-	if len(links[start]) == 0 {
-		return 1
-	}
-	mark[start] = true
-	subHeight := make([]int, len(links[start]))
-	for i := range links[start] {
-		subHeight = append(subHeight, findMinHeightTreesBFS(n, links, links[start][i], mark)+1)
-	}
-	return maths.Max(subHeight...)
+	return []int(leaves)
 }
