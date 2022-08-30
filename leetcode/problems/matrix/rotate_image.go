92b38b13aff65360eeec441497e8d9c5af060047
lovung <vulong1602@gmail.com>
2022-08-31 00:02:44 +0800

    [Leetcode][Medium] 48. Rotate Image

---
+++ leetcode/problems/matrix/rotate_image.go
@@ -0,0 +1,12 @@
+package matrix
+
+// Link: https://leetcode.com/problems/rotate-image/
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-i-1; j++ {
+			matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] =
+				matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
+		}
+	}
+}
