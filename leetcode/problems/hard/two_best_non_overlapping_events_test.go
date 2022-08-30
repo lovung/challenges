ad60d8419ed66d5462950fcb9090e7bced74d760
lovung <vulong1602@gmail.com>
2022-08-30 23:44:28 +0800

    [Leetcode][Medium] 2054. Two Best Non-Overlapping Events

--- leetcode/problems/hard/two_best_non_overlapping_events_test.go
+++ leetcode/problems/hard/two_best_non_overlapping_events_test.go
@@ -1,11 +1,12 @@
 package hard
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_maxValue(t *testing.T) {
+func Test_maxTwoEvents(t *testing.T) {
 	type args struct {
 		events [][]int
-		k      int
 	}
 	tests := []struct {
 		name string
@@ -16,7 +17,6 @@ func Test_maxValue(t *testing.T) {
 			name: "test0",
 			args: args{
 				events: [][]int{},
-				k:      1,
 			},
 			want: 0,
 		},
@@ -24,7 +24,6 @@ func Test_maxValue(t *testing.T) {
 			name: "test1",
 			args: args{
 				events: [][]int{{1, 2, 4}, {3, 4, 3}, {2, 3, 1}},
-				k:      2,
 			},
 			want: 7,
 		},
@@ -32,39 +31,48 @@ func Test_maxValue(t *testing.T) {
 			name: "test2",
 			args: args{
 				events: [][]int{{1, 2, 4}, {3, 4, 3}, {2, 3, 10}},
-				k:      2,
 			},
 			want: 10,
 		},
 		{
 			name: "test3",
 			args: args{
-				events: [][]int{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4}},
-				k:      3,
+				events: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}},
 			},
-			want: 9,
+			want: 1,
 		},
 		{
 			name: "test4",
 			args: args{
-				events: [][]int{{1, 1, 4}, {2, 5, 10}, {6, 6, 1}, {1, 2, 7}, {3, 4, 4}, {5, 6, 5}},
-				k:      4,
-			},
-			want: 16,
-		},
-		{
-			name: "test5",
-			args: args{
 				events: [][]int{{1, 3, 2}, {4, 5, 2}, {2, 4, 3}},
-				k:      2,
 			},
 			want: 4,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxValue(tt.args.events, tt.args.k); got != tt.want {
-				t.Errorf("maxValue() = %v, want %v", got, tt.want)
+			if got := maxTwoEvents(tt.args.events); got != tt.want {
+				t.Errorf("maxTwoEvents() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildEventKey(t *testing.T) {
+	type args struct {
+		event []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildEventKey(tt.args.event); got != tt.want {
+				t.Errorf("buildEventKey() = %v, want %v", got, tt.want)
 			}
 		})
 	}
