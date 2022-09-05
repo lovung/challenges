06a7fdd2ac147139a2f3753d9ce6dea79852b377
lovung <vulong1602@gmail.com>
2022-09-05 18:09:08 +0800

    [Leetcode][Hard] 987. Vertical Order Traversal of a Binary Tree

--- leetcode/problems/binarytree/vertical_order_traversal_of_a_binary_tree.go
+++ leetcode/problems/binarytree/vertical_order_traversal_of_a_binary_tree.go
@@ -1,5 +1,11 @@
 package binarytree
 
+import (
+	"sort"
+
+	"github.com/lovung/ds/trees"
+)
+
 // Link: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
 /**
  * Definition for a binary tree node.
@@ -10,6 +16,58 @@ package binarytree
  * }
  */
 
-// func verticalTraversal(root *trees.TreeNode[int]) [][]int {
+type nodeWithLevel struct {
+	val    int
+	vl, hl int
+}
 
-// }
+func verticalTraversal(root *trees.TreeNode[int]) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	levels := make([]*nodeWithLevel, 0)
+	recursiveTraversal(root, 0, 0, &levels)
+	sort.Slice(levels, func(i, j int) bool {
+		if levels[i].hl < levels[j].hl {
+			return true
+		}
+		if levels[i].hl > levels[j].hl {
+			return false
+		}
+		// don't need to check because we travel top->down
+		// if levels[i].vl < levels[j].vl {
+		// 	return true
+		// }
+		if levels[i].vl > levels[j].vl {
+			return false
+		}
+		return levels[i].val < levels[j].val
+	})
+
+	curHL := levels[0].hl
+	runHL := 0
+	res := make([][]int, 0, levels[len(levels)-1].hl-curHL+1)
+	res = append(res, make([]int, 0))
+	for i := range levels {
+		for curHL < levels[i].hl {
+			res = append(res, make([]int, 0))
+			runHL++
+			curHL++
+		}
+		res[runHL] = append(res[runHL], levels[i].val)
+	}
+	return res
+}
+
+func recursiveTraversal(node *trees.TreeNode[int], vLevel, hLevel int, levels *[]*nodeWithLevel) {
+	if node == nil {
+		return
+	}
+	*levels = append(*levels, &nodeWithLevel{
+		val: node.Val,
+		vl:  vLevel,
+		hl:  hLevel,
+	})
+	recursiveTraversal(node.Left, vLevel+1, hLevel-1, levels)
+	recursiveTraversal(node.Right, vLevel+1, hLevel+1, levels)
+}
--- leetcode/problems/binarytree/vertical_order_traversal_of_a_binary_tree_test.go
+++ leetcode/problems/binarytree/vertical_order_traversal_of_a_binary_tree_test.go
@@ -1,33 +1,73 @@
 package binarytree
 
-// func Test_verticalTraversal(t *testing.T) {
-// 	type args struct {
-// 		root *trees.TreeNode[int]
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want [][]int
-// 	}{
-// 		{
-// 			args: args{
-// 				root: trees.Slice2TreeNode[int](
-// 					[]*int{
-// 						pointer.Of(3), pointer.Of(9), pointer.Of(20),
-// 						nil, nil, pointer.Of(15), pointer.Of(7),
-// 					},
-// 				),
-// 			},
-// 			want: [][]int{
-// 				{9}, {3, 15}, {20}, {7},
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := verticalTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("verticalTraversal() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+import (
+	"reflect"
+	"testing"
+
+	"github.com/lovung/ds/pointer"
+	"github.com/lovung/ds/trees"
+)
+
+func Test_verticalTraversal(t *testing.T) {
+	type args struct {
+		root *trees.TreeNode[int]
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{
+				root: trees.Slice2TreeNode(
+					[]*int{
+						pointer.Of(3), pointer.Of(9), pointer.Of(20),
+						nil, nil, pointer.Of(15), pointer.Of(7),
+					},
+				),
+			},
+			want: [][]int{
+				{9}, {3, 15}, {20}, {7},
+			},
+		},
+		{
+			args: args{
+				root: nil,
+			},
+			want: [][]int{},
+		},
+		{
+			args: args{
+				root: trees.Slice2TreeNode(
+					[]*int{
+						pointer.Of(1), pointer.Of(2), pointer.Of(3),
+						pointer.Of(4), pointer.Of(5), pointer.Of(6), pointer.Of(7),
+					},
+				),
+			},
+			want: [][]int{
+				{4}, {2}, {1, 5, 6}, {3}, {7},
+			},
+		},
+		{
+			args: args{
+				root: trees.Slice2TreeNode(
+					[]*int{
+						pointer.Of(1), pointer.Of(2), pointer.Of(3),
+						pointer.Of(4), pointer.Of(6), pointer.Of(5), pointer.Of(7),
+					},
+				),
+			},
+			want: [][]int{
+				{4}, {2}, {1, 5, 6}, {3}, {7},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := verticalTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("verticalTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
