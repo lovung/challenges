62a3bf7c24c6432e2c39da41b8bfa3718dea56a2
lovung <vulong1602@gmail.com>
2022-09-17 21:10:59 +0800

    [Leetcode][Hard] 336. Palindrome Pairs

---
+++ leetcode/problems/hard/palindrome_pairs_test.go
@@ -0,0 +1,43 @@
+package hard
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_palindromePairs(t *testing.T) {
+	type args struct {
+		words []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			args: args{
+				words: []string{"abcd", "dcba", "lls", "s", "sssll"},
+			},
+			want: [][]int{{0, 1}, {1, 0}, {2, 4}, {3, 2}},
+		},
+		{
+			args: args{
+				words: []string{"bat", "tab", "cat"},
+			},
+			want: [][]int{{0, 1}, {1, 0}},
+		},
+		{
+			args: args{
+				words: []string{"a", ""},
+			},
+			want: [][]int{{0, 1}, {1, 0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := palindromePairs(tt.args.words); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("palindromePairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
