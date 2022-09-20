382a52ac814003b3dc7c27ee3de7989fcee7b0dd
Drake <drake.vulong@shopee.com>
2022-09-20 22:05:55 +0800

    [Leetcode][Medium] 718. Maximum Length of Repeated Subarray

--- leetcode/contest/310/4_longest_increasing_subsequence_ii.go
+++ leetcode/contest/310/4_longest_increasing_subsequence_ii.go
@@ -5,12 +5,66 @@ import "github.com/lovung/ds/maths"
 // Link: https://leetcode.com/problems/longest-increasing-subsequence-ii/
 func lengthOfLIS(nums []int, k int) int {
 	lis := make([]int, 1e5+1)
+	ans := 0
 	for i := range nums {
-		if nums[i] >= k {
-			lis[nums[i]] = maths.Max(lis[nums[i]-k:nums[i]]...) + 1
-		} else {
-			lis[nums[i]] = maths.Max(lis[:nums[i]]...) + 1
-		}
+		l := maths.Max(0, nums[i]-k)
+		lis[nums[i]] = maths.Max(lis[l:nums[i]]...) + 1
+		ans = maths.Max(ans, lis[nums[i]])
 	}
-	return maths.Max(lis...)
+	return ans
+}
+
+func lengthOfLIS2(nums []int, k int) int {
+	ans := 0
+	seg := newSegmentTree(1e5 + 1)
+	for i := range nums {
+		l := maths.Max(0, nums[i]-k)
+		r := nums[i]
+		res := seg.query(l, r) + 1
+		ans = maths.Max(res, ans)
+		seg.update(nums[i], res)
+	}
+	return ans
+}
+
+type segmentTree struct {
+	data []int
+	size int
+}
+
+func newSegmentTree(size int) segmentTree {
+	return segmentTree{
+		data: make([]int, size*2),
+		size: size,
+	}
+}
+
+func (st *segmentTree) query(lo, hi int) int {
+	lo += st.size
+	hi += st.size
+
+	res := 0
+	for lo < hi {
+		if lo&1 != 0 {
+			res = maths.Max(res, st.data[lo])
+			lo++
+		}
+		if hi&1 != 0 {
+			hi--
+			res = maths.Max(res, st.data[hi])
+		}
+		lo >>= 1
+		hi >>= 1
+	}
+	return res
+}
+
+func (st *segmentTree) update(pos, val int) {
+	pos += st.size
+	st.data[pos] = val
+
+	for pos > 1 {
+		pos >>= 1
+		st.data[pos] = maths.Max(st.data[2*pos], st.data[2*pos+1])
+	}
 }
