38ffb4f059f5949d1e28f106778b519a347c8fdd
lovung <vulong1602@gmail.com>
2022-09-27 21:00:47 +0800

    [Leetcode][Grind75] 208. Implement Trie (Prefix Tree)

---
+++ leetcode/grind75/implement_trie_prefix_tree_test.go
@@ -0,0 +1,25 @@
+package grind75
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Link: https://leetcode.com/problems/implement-trie-prefix-tree/
+func TestTrie(t *testing.T) {
+	t.Run("apple", func(t *testing.T) {
+		trie := Constructor()
+		trie.Insert("apple")
+		assert.True(t, trie.Search("apple"))
+		assert.False(t, trie.Search("app"))
+		assert.True(t, trie.StartsWith("app"))
+		trie.Insert("app")
+		assert.True(t, trie.Search("app"))
+		assert.False(t, trie.Search("applepay"))
+	})
+	t.Run("a", func(t *testing.T) {
+		trie := Constructor()
+		assert.False(t, trie.StartsWith("a"))
+	})
+}
