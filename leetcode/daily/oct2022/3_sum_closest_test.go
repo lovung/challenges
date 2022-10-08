ae8f15c901b12731b883c35f9612f6d28c644e22
lovung <vulong1602@gmail.com>
2022-10-08 12:18:14 +0800

    [Leetcode][Daily] 16. 3Sum Closest

---
+++ leetcode/daily/oct2022/3_sum_closest_test.go
@@ -0,0 +1,37 @@
+package oct2022
+
+import "testing"
+
+func Test_threeSumClosest(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{
+				nums:   []int{-1, 2, 1, -4},
+				target: 1,
+			},
+			want: 2,
+		},
+		{
+			args: args{
+				nums:   []int{0, 0, 0},
+				target: 1,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("threeSumClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
