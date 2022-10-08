e59da8423090625e287173123d3ee717aa164595
lovung <vulong1602@gmail.com>
2022-10-08 22:46:02 +0800

    [Leetcode][Grind75] 127. Word Ladder

---
+++ leetcode/grind75/word_ladder.go
@@ -0,0 +1,55 @@
+package grind75
+
+import "github.com/lovung/ds/queue"
+
+// Link: https://leetcode.com/problems/word-ladder/
+type wordQueueItem struct {
+	word    string
+	stepCnt int
+}
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordMap := make(map[string]bool)
+	for i := range wordList {
+		wordMap[wordList[i]] = true
+	}
+	visited := make(map[string]bool)
+	q := queue.NewQueue[*wordQueueItem]()
+	q.Push(&wordQueueItem{
+		word:    beginWord,
+		stepCnt: 0,
+	})
+	for q.Len() > 0 {
+		w := q.Pop()
+		if w.word == endWord {
+			return w.stepCnt + 1
+		}
+		transform1CharWithCheck(w.word, wordMap, w.stepCnt, &q, visited)
+	}
+	return 0
+}
+
+func transform1CharWithCheck(
+	word string, wordMap map[string]bool, stepCnt int,
+	q *queue.Queue[*wordQueueItem], visited map[string]bool,
+) {
+	for i := range word {
+		for j := byte(0); j < 26; j++ {
+			newWord := []byte(word)
+			newWord[i] = a + j
+			strNewWord := string(newWord)
+			if strNewWord == word {
+				continue
+			}
+			if wordMap[strNewWord] {
+				if !visited[strNewWord] {
+					q.Push(&wordQueueItem{
+						word:    strNewWord,
+						stepCnt: stepCnt + 1,
+					})
+					visited[strNewWord] = true
+				}
+			}
+		}
+	}
+}
