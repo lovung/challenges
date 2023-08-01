36939780d8e4ca7666b79093870735450fbd1a37
lovung <vulong1602@gmail.com>
2023-08-01 22:59:21 +0800

    [Leetcode][Daily] 20230801 - Combinations

---
+++ leetcode/daily/aug2023/combinations.go
@@ -0,0 +1,28 @@
+package aug2023
+
+// https://leetcode.com/problems/combinations/
+func combine(n int, k int) [][]int {
+	ret := make([][]int, 0)
+	allItems := make([]int, 0, n)
+	for i := 1; i <= n; i++ {
+		allItems = append(allItems, i)
+	}
+	recursiveCombine(allItems, []int{}, k, &ret)
+	return ret
+}
+
+func recursiveCombine(remainItems []int, currentItems []int, k int, ret *[][]int) {
+	if len(currentItems) == k {
+		*ret = append(*ret, currentItems)
+		return
+	}
+	for i := range remainItems {
+		newRemainItems := make([]int, len(remainItems)-i-1)
+		copy(newRemainItems, remainItems[i+1:])
+
+		newCurrentItems := make([]int, len(currentItems)+1)
+		copy(newCurrentItems, currentItems)
+		newCurrentItems[len(newCurrentItems)-1] = remainItems[i]
+		recursiveCombine(newRemainItems, newCurrentItems, k, ret)
+	}
+}
