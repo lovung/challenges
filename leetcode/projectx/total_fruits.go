65a810d79017a2656c6628e830ee9298b34bc87f
lovung <vulong1602@gmail.com>
2024-03-03 11:18:47 +0800

    347. Top K Frequent Elements

--- leetcode/projectx/total_fruits.go
+++ leetcode/projectx/total_fruits.go
@@ -5,23 +5,23 @@ func totalFruit(fruits []int) int {
 	const baskets = 2
 
 	typesCounter := make(map[int]int)
-	numOfTypesInRange := 0
-	maxSubArrLen := 0
+	numOfTypes := 0
+	maxFruitTypes := 0
 
 	for l, r := 0, 0; r < len(fruits); r++ {
-		for ; numOfTypesInRange > baskets; l++ {
+		for ; numOfTypes > baskets; l++ {
 			typesCounter[fruits[l]]--
 			if typesCounter[fruits[l]] == 0 {
-				numOfTypesInRange--
+				numOfTypes--
 			}
 		}
 		typesCounter[fruits[r]]++
 		if typesCounter[fruits[r]] == 1 {
-			numOfTypesInRange++
+			numOfTypes++
 		}
-		if numOfTypesInRange <= baskets {
-			maxSubArrLen = max(maxSubArrLen, r-l+1)
+		if numOfTypes <= baskets {
+			maxFruitTypes = max(maxFruitTypes, r-l+1)
 		}
 	}
-	return maxSubArrLen
+	return maxFruitTypes
 }
